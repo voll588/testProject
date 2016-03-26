59da745fe470408868226fd5a81a7fddb2d896a4
dandanhuiyi <y.bing@msn.com>
2016-03-26 16:53:46 +0800

    add devices

---
+++ app/js/controller/devices/devicesController.js
@@ -0,0 +1,7 @@
+/**
+ * Created by lost on 2016/3/26.
+ */
+App.controller("DevicesController",['$rootScope','$scope','$filter','$http','$cookieStore','$state',function($rootScope,$scope,$filter,$http,$cookieStore,$state){
+
+
+}]);
